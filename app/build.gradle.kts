c0627d374de39bfe2033ff5f534b289afa78e837
HaohanHe <hehaohan@hsyscn.top>
2026-07-22 20:13:58 +0800

    Revert "chore(release): bump version to 1.2.1-live (versionCode 4) for CCTV live demo"
    This reverts commit 4cedb947d768cbe2ced7a5de3049b14b8f8da7ce.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "top.hsyscn.opedrgent"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2.1-live"
+        versionCode = 3
+        versionName = "1.2sat"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
